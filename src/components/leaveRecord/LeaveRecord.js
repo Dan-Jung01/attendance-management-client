05d1cab94a6bef1399ed850034db8e76cd46cacd
정대한 <daehan.jung01@gmail.com>
2022-06-01 20:58:20 +0900

    Feat: 테이블 컬럼 데이터 변경

--- src/components/leaveRecord/LeaveRecord.js
+++ src/components/leaveRecord/LeaveRecord.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "css/workRecord.css";
+import "css/leaveRecord.css";
 import { Table, TableBody, TableContainer, TableHead, TableCell, TableRow } from "@material-ui/core";
 import axios from "axios";
 
@@ -11,15 +11,15 @@ const LeaveRecord = ({ userId }) => {
   const eachRow = () =>
     leaveRecord.map((row, i) => {
       return (
-        <TableRow key={i}>
-          <TableCell component="th" scope="row" align="center">
-            {row.today_date}
-          </TableCell>
+        <TableRow key={i} className="tb-row">
           <TableCell component="th" scope="row" align="center">
             {row.start_date}
           </TableCell>
           <TableCell component="th" scope="row" align="center">
             {row.end_date}
+          </TableCell>
+          <TableCell component="th" scope="row" align="center">
+            {row.reason}
           </TableCell>
           <TableCell component="th" scope="row" align="center">
             {row.status}
@@ -45,10 +45,10 @@ const LeaveRecord = ({ userId }) => {
     }
   }, [userId]);
 
-  const columns = ["날짜", "출근시간", "퇴근시간"];
+  const columns = ["시작일", "종료일", "사유", "요청 상태"];
 
   return (
-    <div className="work-record-container">
+    <div className="leave-record-container">
       <TableContainer style={{ height: "auto", maxHeight: 300 }} className="tb-isOnWork">
         <section className="tb-title">
           <div>연차사용기록</div>
