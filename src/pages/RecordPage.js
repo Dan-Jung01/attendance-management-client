9bfedfe97305e9bf02da3c61e4dc75c53ab7f8cc
정대한 <daehan.jung01@gmail.com>
2022-06-01 21:01:25 +0900

    Feat: 연차기록 컴포넌트 추가

--- src/pages/RecordPage.js
+++ src/pages/RecordPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Container from "../components/Container";
 import WorkRecord from "../components/workRecord/WorkRecord";
+import LeaveRecord from "components/leaveRecord/LeaveRecord";
 
 const RecordPage = ({ userName, userId }) => {
   return (
     <Container>
       <WorkRecord userName={userName} userId={userId} />
+      <LeaveRecord userName={userName} userId={userId} />
     </Container>
   );
 };
