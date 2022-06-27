efa51229e03d55df7da3fde912080f2bf392c78c
정대한 <daehan.jung01@gmail.com>
2022-06-27 15:21:54 +0900

    Design: activeClass 작동 구현

--- src/admin/components/SideNav.js
+++ src/admin/components/SideNav.js
@@ -9,26 +9,26 @@ const SideNav = () => {
     <div>
       <div className="side-container">
         <div className="menu">
-          <NavLink to="/admin" className="link" style={({ isActive }) => ({ color: isActive ? "organge" : null })}>
+          <NavLink to="/admin" className="link" end style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
             <div>Current Status</div>
           </NavLink>
 
-          <NavLink to="work" className="link" style={({ isActive }) => ({ color: isActive ? "organge" : null })}>
+          <NavLink to="work" className="link" end style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
             <div>Work History</div>
           </NavLink>
 
-          <NavLink to="leave" className="link" style={({ isActive }) => ({ color: isActive ? "organge" : null })}>
+          <NavLink to="leave" className="link" style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
             <div>Leave History</div>
           </NavLink>
 
-          <NavLink to="users" className="link" style={({ isActive }) => ({ color: isActive ? "organge" : null })}>
+          <NavLink to="users" className="link" style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
             <div>User Management</div>
           </NavLink>
 
-          <NavLink to="register" className="link" style={({ isActive }) => ({ color: isActive ? "organge" : null })}>
+          <NavLink to="register" className="link" style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
             <div>Add User</div>
           </NavLink>
-          <NavLink to="" className="link">
+          <NavLink to="/" end className="link">
             <div onClick={() => logout(() => console.log("Logged out succesfully"))}>Logout</div>
           </NavLink>
         </div>
