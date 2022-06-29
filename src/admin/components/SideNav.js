429d60f6cf2faf4a3a20cd111e2993e741205b41
정대한 <daehan.jung01@gmail.com>
2022-06-29 10:05:40 +0900

    Design: 어드민 페이지 사이드 메뉴 디자인 변경

--- src/admin/components/SideNav.js
+++ src/admin/components/SideNav.js
@@ -1,39 +1,55 @@
-import { useAuthContext } from "providers/AuthProvider";
-import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "providers/AuthProvider";
 import "../css/sideNav.css";
+import { MdLogout, MdOutlineDashboard, MdAccessTime, MdOutlineCalendarToday } from "react-icons/md";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { RiUserSettingsLine } from "react-icons/ri";
 
 const SideNav = () => {
   const { logout } = useAuthContext();
   return (
-    <div>
+    <nav>
       <div className="side-container">
-        <div className="menu">
-          <NavLink to="/admin" className="link" end style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
-            <div>Current Status</div>
+        <div className="section logo">LLOOGG</div>
+        <div className="section menu">
+          <NavLink to="/admin" className="link" end>
+            <div className="list">
+              <MdOutlineDashboard className="icons" />
+              <div>대시보드</div>
+            </div>
           </NavLink>
 
-          <NavLink to="work" className="link" end style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
-            <div>Work History</div>
+          <NavLink to="work" className="link">
+            <div className="list">
+              <MdAccessTime className="icons" />
+              <div>출퇴근 관리</div>
+            </div>
           </NavLink>
 
-          <NavLink to="leave" className="link" style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
-            <div>Leave History</div>
+          <NavLink to="leave" className="link">
+            <div className="list">
+              <MdOutlineCalendarToday className="icons" />
+              <div>연차 관리</div>
+            </div>
           </NavLink>
 
-          <NavLink to="users" className="link" style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
-            <div>User Management</div>
+          <NavLink to="users" className="link">
+            <div className="list">
+              <RiUserSettingsLine className="icons" /> <div>사용자 관리</div>
+            </div>
           </NavLink>
 
-          <NavLink to="register" className="link" style={({ isActive }) => ({ color: isActive ? "orange" : null })}>
-            <div>Add User</div>
-          </NavLink>
-          <NavLink to="/" end className="link">
-            <div onClick={() => logout(() => console.log("Logged out succesfully"))}>Logout</div>
+          <NavLink to="register" className="link">
+            <div className="list">
+              <AiOutlineUserAdd className="icons" /> <div>사용자 추가</div>
+            </div>
           </NavLink>
         </div>
+        <div className="section link btn-logout" onClick={() => logout(() => console.log("Logged out succesfully"))}>
+          <MdLogout className="icons" /> <div>로그아웃</div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
