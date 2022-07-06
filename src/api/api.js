9c1ba84a5921d44873a4dc5914064b9a52b7d344
정대한 <daehan.jung01@gmail.com>
2022-07-06 14:14:49 +0900

    Fix: api baseURL 변경

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import { create } from "apisauce";
 
 const api = create({
-  baseURL: "https://52.78.201.2:3003",
+  // baseURL: "https://52.78.201.2:3003",
+  baseURL: "https://scglove-work.com",
 });
 
 export default api;
